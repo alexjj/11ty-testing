7b6d5c0aa326418393d76f49dd2b6e24ba848b16
Alex Johnstone <alexjj@gmail.com>
2024-01-21 21:39:08 +0000

    updates

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,14 @@
+const Image = require("@11ty/eleventy-img");
+
 const dayjs = require('dayjs');
 var advancedFormat = require('dayjs/plugin/advancedFormat')
 dayjs.extend(advancedFormat)
 
 module.exports = function(eleventyConfig) {
-	// Copy the contents of the `public` folder to the output folder
-	// For example, `./public/css/` ends up in `_site/css/`
+	// Copy the contents of the `assets` folder to the output folder
+	// For example, `./assets/css/` ends up in `_site/css/`
 	eleventyConfig.addPassthroughCopy({
-		"./public/": "/",
+		"./assets/": "/",
 	});
 
 	// Filters
@@ -14,5 +16,21 @@ module.exports = function(eleventyConfig) {
         return dayjs(dateObj).format(format || 'dddd, Do MMMM, YYYY')
 	});
 
+	// image plugin
+	eleventyConfig.addShortcode("image", async function(src, alt, sizes) {
+		let metadata = await Image(src, {
+			widths: [300, 570, 880, 1200],
+			formats: ["avif", "webp", "jpeg"]
+		});
 
+		let imageAttributes = {
+			alt,
+			sizes,
+			loading: "lazy",
+			decoding: "async",
+		};
+
+		// You bet we throw an error on a missing alt (alt="" works okay)
+		return Image.generateHTML(metadata, imageAttributes);
+	});
 }
